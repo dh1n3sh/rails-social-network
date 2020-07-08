24ef4d69847dab28b6edc1c51e1134bf22f57bbe
dhinesh <dhinesh@leankloud.com>
2020-07-08 21:07:36 +0530

    added tests for profiles controller

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
-
+require 'profiles_controller'
 class ProfilesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  test "should get index" do
+    get profiles_url
+    assert_response :success
+  end
+
+  test "should create and profile" do
+    assert_difference("Profile.count") do
+      post profiles_url, params: { profile: { name: "Rex"} }
+    end
+
+    assert_redirected_to profiles_path
+  end
 end
