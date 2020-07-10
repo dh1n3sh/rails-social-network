e7e1db129514a5209bc967dade486f3efce00f0c
dhinesh <dhinesh@leankloud.com>
2020-07-10 11:12:40 +0530

    added tests for controllers

--- test/controllers/profiles_controller_test.rb
+++ test/controllers/profiles_controller_test.rb
@@ -9,11 +9,28 @@ class ProfilesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create and profile" do
+  test "should create profile with skill" do
     assert_difference("Profile.count") do
-      post profiles_url, params: { profile: { name: "Rex"} }
+      post profiles_url, params: { profile: { name: "Rex", skills:{skill_id: 1} }}
     end
-
+    prof = Profile.find_by(name: "Rex")
+    skill1 = Skill.find(1)
+    assert_equal prof.skills.first.name, skill1.name
     assert_redirected_to profiles_path
   end
+
+  test "should delete profile" do
+    id = Profile.create(name: 'test1').id
+    delete profile_path(id)
+    assert_raises(ActiveRecord::RecordNotFound) { 
+      Profile.find(id)
+    }
+  end
+
+  test "should update profile" do
+    id = Profile.create(name: 'test2').id
+    patch profile_path(id), params: { profile: { name: 'test3'}}
+    assert_equal Profile.find(id).name, 'test3'
+  end
+
 end
